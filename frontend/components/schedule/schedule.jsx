8ea5f466bf0dd84eb97b4abf187ee5242d10c0e2
echu18 <elizabethchu18@gmail.com>
2020-07-29 14:20:37 -0700

    update schedule column width settings

--- frontend/components/schedule/schedule.jsx
+++ frontend/components/schedule/schedule.jsx
@@ -87,23 +87,35 @@ class Schedule extends React.Component {
         let time = new Date(workOrder.time)
         let hours = time.getHours();
         let minutes = time.getMinutes();
+        let year = time.getFullYear()
+        let month = time.getMonth()
+        let date = time.getDate()
+
+
         let newTime = new Date(time.getTime()+1000*60*60*(workOrder.duration/60))
+        let newHours = newTime.getHours();
+        let newMinutes = newTime.getMinutes();
+        let newYear = newTime.getFullYear();
+        let newMonth = newTime.getMonth();
+        let newDate = newTime.getDate();
+
         let location = locations[workOrder.location_id];
         
         
         let title = (
           <div>
             {/* <p>{`Start at: ${hours}:${minutes}`}</p> */}
-            <p>{`Start at: ${time}`}</p>
-            {/* <p>{`End at: ${newTime}`}</p>
-            <p>{`Duration: ${workOrder.duration}`}</p>
+            {/* <p>{`Start at: ${time}`}</p> */}
+            {/* <p>{`End at: ${newTime}`}</p> */}
+            <p>{`Duration: ${workOrder.duration} mins`}</p>
             <p>{`Location: ${location.name}`}</p>
-            <p>{`City: ${location.city}`}</p> */}
+            <p>{`City: ${location.city}`}</p>
+            <p>{`Price: $${workOrder.price}`}</p>
           </div>
           // `Start at ${time}
           // Location: ${location.name}
           // City: ${location.city}`
-        );
+          );
         
           events.push({
             id: `work-order-${workOrder.id}`,
@@ -112,8 +124,8 @@ class Schedule extends React.Component {
             resourceId: `tech-${workOrder.technician_id}`,
             // start: time,
             // end: (time + workOrder.duration),
-            start: new Date(2020, 6, 28, 10, 30),
-            end: new Date(2020, 6, 28, 12, 0),
+            start: new Date(year, month, date, hours, minutes),
+            end: new Date(newYear, newMonth, newDate, newHours, newMinutes),
             // desc: `${location.name}-${location.city}`,
           });
         
@@ -168,7 +180,180 @@ class Schedule extends React.Component {
 
 
 
-  const localizer = momentLocalizer(moment);
+    const localizer = momentLocalizer(moment);
+
+
+
+
+  // Resource JSON format:
+  // const resources = [
+  //   {
+  //     id: "a",
+  //     title: `Bill Keller`,
+  //   },
+  //   {
+  //     id: "b",
+  //     title: "Juan Garcia",
+  //   },
+  //   {
+  //     id: "c",
+  //     title: "Room C",
+  //   },
+  // ];
+
+
+  function mapCalResources(technicians){
+    if (!technicians) return;
+    let resources = [];
+
+    for (let i=0; i < technicians.length; i++){
+      let technician = technicians[i];
+
+      resources.push({id:`tech-${technician.id}`, title: `${technician.name}`})
+    }
+    return resources;
+  }
+
+
+  function dayStartTime(){
+    let date = new Date();
+      date.setHours(5);
+      date.setMinutes(0);
+      return date;
+  }
+
+  function dayEndTime(){
+    let date = new Date();
+      date.setHours(19);
+      date.setMinutes(0);
+      return date;
+  }
+  
+  const columnClasses = [
+    '.rbc-time-view', '.rbc-row', '.rbc-time-view-resources', '.rbc-day-slot'
+    // '.rbc-time-view', '.rbc-row', '.rbc-time-view-resources', '.rbc-day-slot'
+  ]
+
+    function columnClassElements(){
+      let elementGroup = [];
+
+      for (let i=0; i < columnClasses.length; i++){
+        let elements = document.getElementsByClassName(columnClasses[i])
+
+        elementGroup = elementGroup.concat(Array.from(elements));
+      }
+      return elementGroup;
+    }
+
+
+    let btnGroup = document.getElementsByClassName("rbc-btn-group");
+    // let navBtns = btnGroup[0];
+    let viewBtns = btnGroup[1];
+
+    if (viewBtns) {
+      let children = viewBtns.children;
+
+      for (let i=0; i < children.length; i++){
+        let child = children[i];
+        
+
+        if (child.innerHTML === "Day" && child.className === "rbc-active") {
+            changeColumn('fixed-width');
+        }
+
+
+        switch (child.innerHTML){
+          case 'Day':
+            child.addEventListener('click', e=> {e.preventDefault(); changeColumn('fixed-width')} );
+            child.addEventListener('dblclick', e=> {e.preventDefault(); changeColumn('auto-width')} );
+            break;
+          case 'Week':
+            child.addEventListener('click', e=> {e.preventDefault(); changeColumn('auto-width')} );
+            break;
+          default:
+            child.addEventListener('click', e=> {e.preventDefault(); changeColumn('auto-width')} );
+        }
+
+      } 
+    }
+    
+    function changeColumn(type){
+      let elements = columnClasses;
+
+      switch (type) {
+        case 'fixed-width':
+          mapFixed();
+          break;
+        case 'auto-width':
+          mapAuto();
+          break;
+        default:
+          mapAuto();
+      }
+
+        function mapFixed(){
+          for (let j=0; j< elements.length; j++){
+            $(elements[j]).css("min-width", "")
+            $(elements[j]).css("min-width", "450px")
+          }         
+        }
+        
+        function mapAuto(){
+          for (let j=0; j< elements.length; j++){
+            $(elements[j]).css("min-width", "")
+            $(elements[j]).css("width", "100%")
+          }         
+        }
+    }
+  
+
+
+  
+    return (
+      <div>
+        <h3>Schedule</h3>
+
+        {this.state.workOrders.length > 0 ? (
+          <Calendar
+            resources={mapCalResources(this.state.technicians)}
+            // events={[mapCalEvents(this.state.workOrders, this.state.locations)]}
+            events={this.state.events}
+            // events={events}
+            localizer={localizer}
+            culture="en-US"
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={new Date()}
+            views={["day", "week", "month"]}
+            // views={['day', 'week', 'month']}
+            defaultView="day"
+            // step={7.5}
+            min={dayStartTime()}
+            max={dayEndTime()}
+            style={{ height: 800 }}
+            // onSelectEvent={(event) => console.log(event.title.innhe)}
+
+            // onView={changeMinWidth(this.view)}
+          />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+
+export default Schedule;
+
+
+
+
+
+
+
+
+
+
+
 
 
     
@@ -228,92 +413,37 @@ class Schedule extends React.Component {
 
 
 
-  // Resource JSON format:
-  // const resources = [
-  //   {
-  //     id: "a",
-  //     title: `Bill Keller`,
-  //   },
-  //   {
-  //     id: "b",
-  //     title: "Juan Garcia",
-  //   },
-  //   {
-  //     id: "c",
-  //     title: "Room C",
-  //   },
-  // ];
-
-
-  function mapCalResources(technicians){
-    if (!technicians) return;
-    let resources = [];
-
-    for (let i=0; i < technicians.length; i++){
-      let technician = technicians[i];
-
-      resources.push({id:`tech-${technician.id}`, title: `${technician.name}`})
-    }
-    return resources;
-  }
 
 
 
-
-
-  function dayStartTime(){
-    let date = new Date();
-      date.setHours(5);
-      date.setMinutes(0);
-      return date;
-  }
-
-  function dayEndTime(){
-    let date = new Date();
-      date.setHours(19);
-      date.setMinutes(0);
-      return date;
-  }
+    //   let children = viewBtns.children;
+  //   for (let i=0; i < children.length; i++){
+  //     let child = children[i];
+      
+  //     if (child.innerHTML === "Day" && child.className === "rbc-active") {
+  //       let elements = columnClassElements();
+  //       elements.forEach((el) => el.setAttribute("style", "min-width: 250px !important;"));
+  //     }
+      
+  //     // updateCols(children[i])
+  //     if (child.innerHTML === 'Day'){
+  //       child.addEventListener('click', e=> {e.preventDefault(); 
+  //         debugger
+  //         let elements = columnClassElements();
+  //         elements.forEach((el) => el.setAttribute("style", "min-width: 250px !important;"));
+  //       })
+  //     }
+  //   }
+  // };
   
+  // function updateCols(child){
+    
+  //   if (child.innerHTML === 'Day' && child.className === 'rbc-active') {
+  //     // child.addEventListener('click', function(){
+  //       let elements = columnClassElements();
+  //       elements.forEach(el => el.setAttribute("style", "min-width: 250px;"))
+  //       // elements.forEach(el => el.setAttribute("style", "border: 1px solid red;"))
+  //     // })
+  //   }
+  // }
   
-    return (
-      <div>
-        <h3>Schedule</h3>
-
-        {/* <h3>Locations</h3>
-        {this.props.locations.map((location) => {
-          return (
-            <div>
-              <p>
-                {location.name} - {location.city}
-              </p>
-            </div>
-          );
-        })}
-
-        <h3>Technicians</h3> */}
-      {this.state.workOrders.length > 0 ? (
-        <Calendar
-          resources={mapCalResources(this.state.technicians)}
-          // events={[mapCalEvents(this.state.workOrders, this.state.locations)]}
-          events={this.state.events}
-          // events={events}
-          localizer={localizer}
-          culture="en-US"
-          startAccessor="start"
-          endAccessor="end"
-          defaultDate={new Date()}
-          views={["day", "week", "month"]}
-          defaultView="day"
-          step={7.5}
-          min={dayStartTime()}
-          max={dayEndTime()}
-          style={{ height: 800 }}
-        />
-    ) : null}
-      </div>
-    );
-  }
-}
-
-export default Schedule;
